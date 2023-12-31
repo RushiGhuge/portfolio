515da7bbfba5e6751073b715f9163065f3a8e768
rushikeshghuge <rushikeshghuge993@gmail.com>
2023-12-31 10:56:23 +0530

    hi

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "portfolio",
+  base: "/portfolio/",
   esbuild: {
     jsxInject: `import React from 'react';`,
   },
